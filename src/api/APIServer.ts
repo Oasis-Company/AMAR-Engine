1cba67d401fa910b79f562b3a3fc335b9c41cffa
CeaserZhao <chkevin.zhao@gmail.com>
2026-02-08 13:41:58 +0800

    Update APIServer.ts

--- src/api/APIServer.ts
+++ src/api/APIServer.ts
@@ -117,4 +117,10 @@ class APIServer {
     // Get a specific metaclass
     router.get('/:id', (req: Request, res: Response) => {
       const { id } = req.params;
-      const
+      const metaclass = this.engine.getMetaclassSystem().getMetaclass(id);
+      
+      if (!metaclass) {
+        return res.status(404).json({ error: `Metaclass ${id} not found` });
+      }
+      
+      res.status(20
