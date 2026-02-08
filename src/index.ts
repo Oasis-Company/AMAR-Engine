35e4d69cf1d7d947233efac87f40196eba8ea28d
CeaserZhao <chkevin.zhao@gmail.com>
2026-02-08 13:39:28 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@
 import { MetaclassSystem } from './core/metaclass/MetaclassSystem';
 import { AEIDSystem } from './core/aeid/AEIDSystem';
 import { SkillsSystem } from './core/skills/SkillsSystem';
+import { MesherSystem } from './core/mesher/MesherSystem';
 
 class AMAREngine {
   private metaclassSystem: MetaclassSystem;
